9a81e419579e487a3bb1ef996f5a7a320e1ca045
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-05-08 10:28:52 -0400

    commit

--- ReadExternalFileDemo.py
+++ ReadExternalFileDemo.py
@@ -4,7 +4,10 @@ names = []
 for line in f:
     if line != '' and line != '\n':
         names.append(line.rstrip()) # rstrip() removes '\n' from end of each string.
-# print(f.read())
-
 verse = input("Which verse would you like to look at? 1-{}: ".format(len(names)-1))
+verse = int(verse)
+if verse == 0 or verse > len(names):
+    print("Verse number is out of range!")
+else:
+    print(names[verse])
 f.close()
