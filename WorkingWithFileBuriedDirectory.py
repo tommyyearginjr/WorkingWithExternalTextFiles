f398bdd675610959ad3e4bf3f7509cc92c039d92
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-05-08 14:17:17 -0400

    Getting there...

--- WorkingWithFileBuriedDirectory.py
+++ WorkingWithFileBuriedDirectory.py
@@ -1,18 +1,18 @@
 # Created 5/8/17 by Tommy H. Yeargin, Jr. Namaste.
-
+from BibleBookIndex import books
 ExternalFile = input('What book of the Bible do you want to read? ')
 ChapterNumber = input('What chapter? ')
-Target = './bible/'+ExternalFile+'/ch'+ChapterNumber+'.txt'
+Target = './bible/{}'.format(books.index(str(ExternalFile)))+'/ch'+ChapterNumber+'.txt'
 Target = str(Target)
 f = open(Target)
-names = []
+VerseList = []
 for line in f:
     if line != '' and line != '\n':
-        names.append(line.rstrip()) # rstrip() removes '\n' from end of each string.
-verse = input("Which verse would you like to look at? 1-{}: ".format(len(names)-1))
+        VerseList.append(line.rstrip()) # rstrip() removes '\n' from end of each string.
+verse = input("Which verse would you like to look at? 1-{}: ".format(len(VerseList)-1))
 verse = int(verse)
-if verse == 0 or verse > len(names):
+if verse == 0 or verse > len(VerseList):
     print("Verse number is out of range!")
 else:
-    print(names[verse])
+    print(VerseList[verse])
 f.close()
